89f8d607bf5d3aa8a53caa7ba704990c8bb4cef1
Becky Resler <1019279+beckyresler@users.noreply.github.com>
2018-09-22 19:51:05 -0500

    added code to hash the password when it is set and not already hashed as well as code to convert timestamps to site timezone

--- app/User.php
+++ app/User.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace App;
 
+use Carbon\Carbon;
+use Config;
+use Hash;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -12,19 +13,47 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for arrays.
-     *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * Encrypts the password attribute when it is set for the model.
+     * @param string $password The password.
+     * @return string the encrypted password.
+     */
+    public function setPasswordAttribute($password)
+    {
+        // does the password still need to be hashed?
+        if(Hash::needsRehash($password))
+            $password = bcrypt($password);
+
+        return $this->attributes['password'] = $password;
+    }
+
+    /**
+	 * Gets the value of the created_at field after converting it to the site timezone.
+	 * @param string $value The value of the created_at attribute from the database. The value is a UTC timestamp.
+	 * @return \Carbon\Carbon A Carbon object converted to the timezone in the site config.
+	 */
+    public function getCreatedAtAttribute($value)
+    {
+    	return Carbon::parse($value)->timezone(Config::get('site.timezone'));
+    }
+
+	/**
+	 * Gets the value of the updated_at field after converting it to the site timezone.
+	 * @param string $value The value of the updated_at attribute from the database. The value is a UTC timestamp.
+	 * @return \Carbon\Carbon A Carbon object converted to the timezone in the site config.
+	 */
+    public function getUpdatedAtAttribute($value)
+    {
+    	return Carbon::parse($value)->timezone(Config::get('site.timezone'));
+    }
 }
