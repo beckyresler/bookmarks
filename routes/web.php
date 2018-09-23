f5e91c076ee30e95e3f44a95c1afaa940afc77e3
Becky Resler <1019279+beckyresler@users.noreply.github.com>
2018-09-23 11:51:52 -0500

    set up auth routes and route groups

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Routes that require authentication - basically everything except the auth routes (below)
+Route::middleware('auth')->group(function(){
+
+	Route::get('/', function () {
+	    return view('welcome');
+	})->name('home');
+
+	// Routes for management functionality
+	Route::prefix('sitemanager')->group(function(){
+
+		Route::get('logout', function(){
+			Auth::logout();
+			return redirect(route('login'))->with('success', 'You have logged out of the system.');
+		})->name('logout');
+	});
+
+});
+
+// Authentication Routes
+Route::prefix('sitemanager')->group(function(){
+
+	Route::get('/', ['uses' => 'Auth\LoginController@showLoginForm'])->name('login');
+	Route::post('login', ['uses' => 'Auth\LoginController@login'])->name('login.process');
+
+	Route::get('password/reset', ['uses' => 'Auth\ForgotPasswordController@showLinkRequestForm'])->name('password.request');
+	Route::post('password/email', ['uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail'])->name('password.email');
+	Route::get('password/reset/{token}', ['uses' => 'Auth\ResetPasswordController@showResetForm'])->name('password.reset');
+	Route::post('password/reset', ['uses' => 'Auth\ResetPasswordController@reset'])->name('password.reset.process');
+
 });
