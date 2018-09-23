182f43169309cb9551e1721ebb640dd697191433
Becky Resler <1019279+beckyresler@users.noreply.github.com>
2018-09-23 10:59:08 -0500

    changed where a user is redirected to after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
@@ -22,18 +21,17 @@ class LoginController extends Controller
 
     /**
      * Where to redirect users after login.
-     *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
-     *
      * @return void
      */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->redirectTo = route('home');
     }
 }
